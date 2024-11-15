7c207a65fca762df82f07417d86814937564b4d8
Admy <dondi1412@gmail.com>
2024-11-15 15:48:07 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.API_PORT;
-const ProjectName = process.env.ProjectName
 const uploadDir = path.join(__dirname, '../uploads');
 
 // Basic authentication middleware
@@ -70,26 +69,35 @@ app.use(express.static('public'));
 app.use(express.static('uploads'));
 
 fs.readdir(uploadDir, { withFileTypes: true }, (err, files) => {
-    const folders = files.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
-    for (let index = 0; index < folders.length; index++) {
-        app.use(`/${ProjectName}`, express.static('uploads/' + folders[index]));
+    const projectNames = files.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+    for (let idx_project = 0; idx_project < projectNames.length; idx_project++) {
+        fs.readdir(path.join(uploadDir, projectNames[idx_project]), { withFileTypes: true }, (err, files) => {
+            const versionNames = files.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+            for (let idx_version = 0; idx_version < versionNames.length; idx_version++) {
+                app.use(`/${projectNames[idx_project]}`, express.static('uploads/' + projectNames[idx_project] + "/" + versionNames[idx_version]));
+            }
+        });
     }
 });
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/manager_page.html'));
+app.get('/project', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/manager_page_project.html'));
 });
 
-app.get('/upload_file/:folder_name', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/project/:project_name', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/manager_page_version.html'));
+});
+
+app.get('/upload_file/:project_name/:version_name', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/upload_file.html'));
 });
 
 // Handle multiple file uploads
-app.post('/upload/:folder_name', (req, res) => {
-    const { folder_name } = req.params;
-    const folderPath = path.join(uploadDir, folder_name as string);
+app.post('/upload/:project_name/:version_name', (req, res) => {
+    const { project_name, version_name } = req.params;
+    const folderPath = path.join(uploadDir, project_name as string, version_name as string);
 
-    console.log('folderPath', folderPath);
+    console.log('upload folderPath', folderPath);
 
     // Create the folder if it doesn't exist
     // if (!fs.existsSync(folderPath)) {
@@ -124,9 +132,9 @@ function formatFileSize(bytes: number): string {
     return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
 }
 // Get list of files
-app.get('/files/:folder_name', (req: Request, res: Response) => {
-    const { folder_name } = req.params;
-    const folderPath = path.join(uploadDir, folder_name as string);
+app.get('/files/:projectName', (req: Request, res: Response) => {
+    const { projectName } = req.params;
+    const folderPath = path.join(uploadDir, projectName as string);
 
     console.log('folderPath', folderPath);
 
@@ -149,11 +157,37 @@ app.get('/files/:folder_name', (req: Request, res: Response) => {
         res.status(200).json({ files: fileDetails });
     });
 });
+app.get('/files/:projectName/:versionName', (req: Request, res: Response) => {
+    const { projectName, versionName } = req.params;
+    const folderPath = path.join(uploadDir, projectName as string, versionName as string);
+
+    console.log('folderPath', folderPath);
+
+    // Create the folder if it doesn't exist
+    // if (!fs.existsSync(folderPath)) {
+    //     fs.mkdirSync(folderPath, { recursive: true });
+    // }
+
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.log('err', err);
+            return res.status(500).json({ message: 'Failed to read directory!' });
+        }
+
+        const fileDetails = files.map((filename) => {
+            const filePath = path.join(folderPath, filename);
+            const stats = fs.statSync(filePath);
+            return { filename, size: formatFileSize(stats.size) };
+        });
+
+        res.status(200).json({ files: fileDetails });
+    });
+});
 
 // Handle file deletion
-app.delete('/delete/:folderName/:filename', (req, res) => {
-    const { folderName, filename } = req.params;
-    const filePath = path.join(uploadDir, folderName, filename);
+app.delete('/delete/:projectName/:versionName/:filename', (req, res) => {
+    const { projectName, versionName, filename } = req.params;
+    const filePath = path.join(uploadDir, projectName, versionName, filename);
 
     fs.unlink(filePath, (err) => {
         if (err) {
@@ -164,9 +198,9 @@ app.delete('/delete/:folderName/:filename', (req, res) => {
 });
 
 // Handle deleting all files
-app.delete('/delete-all/:folderName', (req, res) => {
-    const { folderName } = req.params;
-    const filePath = path.join(uploadDir, folderName);
+app.delete('/delete-all/:projectName/:versionName', (req, res) => {
+    const { projectName, versionName } = req.params;
+    const filePath = path.join(uploadDir, projectName, versionName);
     fs.readdir(filePath, (err, files) => {
         if (err) return res.status(500).json({ message: 'Failed to read directory!' });
 
@@ -193,16 +227,40 @@ app.get('/folders', (req: Request, res: Response) => {
     });
 });
 
+app.get('/folders/:projectName', (req: Request, res: Response) => {
+    const { projectName } = req.params;
+    fs.readdir(path.join(uploadDir, projectName), { withFileTypes: true }, (err, files) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to read directory!' });
+        }
+
+        const folders = files.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+        res.status(200).json({ folders });
+    });
+});
+
 // Handle folder creation
 app.post('/create-folder', (req: Request, res: Response) => {
-    const folderName = req.body.folderName;
-    const folderPath = path.join(uploadDir, folderName);
-    app.use(`/${ProjectName}`, express.static('uploads/' + folderName));
+    const projectName = req.body.projectName;
+    const folderPath = path.join(uploadDir, projectName);
+    app.use(`/${projectName}`, express.static('uploads/' + projectName));
     if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath, { recursive: true });
-        res.status(200).json({ message: 'Folder created successfully!' });
+        res.status(200).json({ message: 'Project created successfully!' });
     } else {
-        res.status(400).json({ message: 'Folder already exists!' });
+        res.status(400).json({ message: 'Project already exists!' });
+    }
+});
+app.post('/create-folder/:projectName', (req: Request, res: Response) => {
+    const { projectName } = req.params;
+    const versionName = req.body.folderName;
+    const folderPath = path.join(uploadDir, projectName, versionName);
+    app.use(`/${projectName}`, express.static('uploads/' + projectName + '/' + versionName));
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+        res.status(200).json({ message: 'Version created successfully!' });
+    } else {
+        res.status(400).json({ message: 'Version already exists!' });
     }
 });
 
@@ -211,6 +269,19 @@ app.post('/create-folder', (req: Request, res: Response) => {
 app.delete('/delete-folder/:folderName', (req: Request, res: Response) => {
     const { folderName } = req.params;
     const folderPath = path.join(uploadDir, folderName);
+
+    if (fs.existsSync(folderPath)) {
+        fs.rmdirSync(folderPath, { recursive: true });
+        res.status(200).json({ message: 'Folder deleted successfully!' });
+    } else {
+        res.status(400).json({ message: 'Folder does not exist!' });
+    }
+});
+
+app.delete('/delete-folder/:projectName/:folderName', (req: Request, res: Response) => {
+    const { projectName } = req.params;
+    const { folderName } = req.params;
+    const folderPath = path.join(uploadDir, projectName, folderName);
 
     if (fs.existsSync(folderPath)) {
         fs.rmdirSync(folderPath, { recursive: true });
